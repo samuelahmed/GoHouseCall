d47237aa57c6e9dfbf16a97992365efb3b94de45
samuelahmed <samueladuval@gmail.com>
2023-05-23 12:41:29 -0700

    meow delete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,15 +18,6 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
        < AuthShowcase />
       </main>
-      <div>
-        meow
-      </div>
-      <div>
-        meow 2
-      </div>
-      <div>
-        meow 3
-      </div>
     </>
   );
 };
@@ -51,7 +42,7 @@ const AuthShowcase: React.FC = () => {
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
         onClick={sessionData ? () => void signOut() : () => void signIn()}
       >
-        {sessionData ? "Sign out" : "Sign in"}
+        {sessionData ? "Sign out" : "Sign in"}  
       </button>
     </div>
   );
