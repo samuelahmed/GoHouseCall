914836641cc475e93e2f07ef647396fa2f36a19c
samuelahmed <samueladuval@gmail.com>
2023-05-25 13:24:14 -0700

    remove unused code and files

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -2,8 +2,8 @@ import { Button } from "~/components/ui/button";
 
 export function Footer() {
   return (
-    <footer className=" h-12 bg-gray4 text-olive12">
-      <div className="flex h-full w-full items-center justify-end py-4">
+    <footer className="h-12 bg-gray4 text-olive12">
+      <div className="flex h-full w-full items-center justify-end px-4 py-4">
         <Button variant="ghost" size="sm">
           Contact
         </Button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,17 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { useSession } from "next-auth/react";
 
-// import RootLayout from "./layout";
-// import Link from "next/link";
-
 const Home: NextPage = () => {
-  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const { data: sessionData } = useSession();
 
-  
   return (
     <>
       <Head>
         <title>House Call</title>
         <meta name="description" content="Connecting patients and caregivers" />
-        <link
-          // rel="iscon" not sure what this is
-          href="/faviconLarge.png"
-        />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center ">
-                {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
       </main>
     </>
   );
