6be2ae730616a30d8ef29d07023c15fc28fc2c93
samuelahmed <samueladuval@gmail.com>
2023-05-24 20:44:30 -0700

    add logo to header and route to home

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,5 +1,7 @@
 import SignInOutTrigger from "~/components/auth/authShowcase";
 import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 import {
   NavigationMenu,
@@ -10,15 +12,26 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-
   navigationMenuTriggerStyle,
 } from "~/components/ui/navigationMenu";
 
 export function Header() {
+  const router = useRouter();
+
   return (
     <header className="">
       <div className="flex h-14 w-full items-center  bg-blue12 px-4">
-        <div className="min-w-fit">House Call</div>
+        <div
+          className="flex min-w-fit items-center space-x-2 px-4 text-xl text-olive1 hover:cursor-pointer"
+          onClick={() => {
+            void router.push("/");
+            // is there a better way to do this?
+          }}
+        >
+          <Image src="/faviconLarge.png" alt="HC Logo" width={30} height={30} />
+          <text>House Call</text>
+        </div>
+
         <div className="w-full text-center">add search bar here</div>
         <div className="min-w-fit">
           <SignInOutTrigger />
@@ -28,15 +41,13 @@ export function Header() {
         <div className="min-w-fit text-olive1">
           <NavigationMenu>
             <NavigationMenuList>
-
-            <NavigationMenuItem>
+              <NavigationMenuItem>
                 <Link href="/docs" legacyBehavior passHref>
                   <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                     Sessions
                   </NavigationMenuLink>
                 </Link>
               </NavigationMenuItem>
-
               <NavigationMenuItem>
                 <Link href="/docs" legacyBehavior passHref>
                   <NavigationMenuLink className={navigationMenuTriggerStyle()}>
@@ -44,8 +55,6 @@ export function Header() {
                   </NavigationMenuLink>
                 </Link>
               </NavigationMenuItem>
-  
-
               <NavigationMenuItem>
                 <Link href="/docs" legacyBehavior passHref>
                   <NavigationMenuLink className={navigationMenuTriggerStyle()}>
