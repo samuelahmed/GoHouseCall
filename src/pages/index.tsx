9e8a07da5e6e36a4da1df42f1fe88f4a2b96d232
samuelahmed <samueladuval@gmail.com>
2023-05-26 15:41:57 -0700

    update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { useSession } from "next-auth/react";
-import { AboutHouseCall } from "~/components/aboutHouseCall";
+import { AboutHouseCall } from "~/components/marketing/aboutHouseCall";
+import { HomeDisplay } from "~/components/marketing/homeDisplay";
 
 const Home: NextPage = () => {
   const { data: sessionData } = useSession();
@@ -12,11 +13,17 @@ const Home: NextPage = () => {
         <title>House Call</title>
         <meta name="description" content="Connecting patients and caregivers" />
       </Head>
-      <div className="min-h-screen px-4 py-4 md:px-8 md:py-8">
-        {!sessionData && <AboutHouseCall />}
+      {!sessionData && (
+        <div className=" ">
+          <HomeDisplay />
+        </div>
+      )}
+      {sessionData && (
 
+      <div className="min-h-screen px-4 py-4 md:px-8 md:py-8">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
       </div>
+      )}
     </>
   );
 };
