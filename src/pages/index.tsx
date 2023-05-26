9d1f0ed26dbad8fe87a5f1700fec6898d3a57291
samuelahmed <samueladuval@gmail.com>
2023-05-26 15:04:39 -0700

    import abouthc page into index and display when no session

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { useSession } from "next-auth/react";
+import { AboutHouseCall } from "~/components/aboutHouseCall";
 
 const Home: NextPage = () => {
   const { data: sessionData } = useSession();
@@ -11,7 +12,9 @@ const Home: NextPage = () => {
         <title>House Call</title>
         <meta name="description" content="Connecting patients and caregivers" />
       </Head>
-      <div className="flex min-h-screen flex-col items-center justify-center ">
+      <div className="min-h-screen px-4 py-4 md:px-8 md:py-8">
+        {!sessionData && <AboutHouseCall />}
+
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
       </div>
     </>
