e183b9ea52f70adf8679318501ab6b435a7d91b1
samuelahmed <samueladuval@gmail.com>
2023-05-27 00:04:43 -0700

    add contact us page and required ui primitives

---
+++ src/components/contactForm.tsx
@@ -0,0 +1,50 @@
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { Textarea } from "~/components/ui/textarea";
+
+export function ContactForm() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Card className=" w-screen">
+        <CardHeader>
+          <CardTitle className="text-center text-xl md:text-2xl lg:text-3xl">
+            Contact Us
+          </CardTitle>
+          <CardDescription>
+            <p>
+              Please fill out the form below and the House Call team will get back to you as soon
+              as possible.
+            </p>
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-6">
+          <div className="grid gap-2">
+            <Label htmlFor="subject">Subject</Label>
+            <Input id="subject" placeholder="I need help with..." />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="description">Description</Label>
+            <Textarea
+              className="h-40"
+              id="description"
+              placeholder="Please include all information relevant to your issue."
+            />
+          </div>
+        </CardContent>
+        <CardFooter className="justify-between space-x-2">
+          <Button variant="ghost">Cancel</Button>
+          <Button>Submit</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
