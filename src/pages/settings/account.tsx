428d16365fe49b0b8087a47b1bcffdb479efb515
samuelahmed <samueladuval@gmail.com>
2023-05-28 22:10:26 -0700

    routing and basic form setup for account

--- src/pages/settings/account.tsx
+++ src/pages/settings/account.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import SettingsLayout from "~/components/settings/settingsLayout";
+import { AccountForm } from "~/components/settings/accountForm";
 
 const Account: NextPage = () => {
   return (
@@ -11,7 +12,7 @@ const Account: NextPage = () => {
       </Head>
       <div className="min-h-screen px-4 py-4 md:px-8 md:py-8">
         <SettingsLayout>
-          Account
+          < AccountForm />
         </SettingsLayout>
       </div>
     </>
