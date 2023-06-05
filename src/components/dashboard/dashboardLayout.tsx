e507748915fdc27079625770bd8f3a48b71661af
samuelahmed <samueladuval@gmail.com>
2023-06-05 15:21:09 -0700

    setup redirect to not look buggy on refresh using server side prop

---
+++ src/components/dashboard/dashboardLayout.tsx
@@ -0,0 +1,27 @@
+import { Separator } from "~/components/ui/separator";
+import { useSession } from "next-auth/react";
+
+
+interface DashboardProps {
+    children: React.ReactNode;
+}
+
+export default function DashboardLayout({ children }: DashboardProps) {
+    const { data: sessionData } = useSession();
+
+  return (
+    <>
+      <div className="min-h-screen overflow-auto  ">
+        <div className="-mb-3 space-y-0.5 ">
+          <h2 className="justify-start text-2xl font-bold tracking-tight">
+            {sessionData?.user?.name}&apos;s Dashboard
+          </h2>
+        </div>
+        <Separator className="my-6" />
+        <div className="flex flex-col space-y-8 px-4 lg:flex-row lg:space-x-12 lg:space-y-0">
+          <div className="flex-1 lg:max-w-2xl">{children}</div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,30 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { useSession } from "next-auth/react";
+import DashboardLayout from "~/components/dashboard/dashboardLayout";
+import Dashboardinfo from "~/components/dashboard/dashboardInfo";
+import { type GetServerSidePropsContext } from "next";
+import { getServerAuthSession } from "../server/auth";
+
+//redict to home page if not logged in
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const session = await getServerAuthSession(ctx);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
 
 const Dashboard: NextPage = () => {
+  const { data: sessionData } = useSession();
+
   return (
     <>
       <Head>
@@ -9,8 +32,18 @@ const Dashboard: NextPage = () => {
         <meta name="description" content="Dashboard for House Call users." />
       </Head>
       <div className="h-screen">
-        {/* <MessagesLayout /> */}
-        dashboard
+        {!sessionData && (
+          <>{/* Logged out users have been redirected to home page */}</>
+        )}
+        {sessionData && (
+          <>
+            <div className="h-screen px-4 py-4">
+              <DashboardLayout>
+                <Dashboardinfo />
+              </DashboardLayout>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
