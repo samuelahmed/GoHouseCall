c3fb5de14e025abed1a051b131ac1eecfc6c980c
samuelahmed <samueladuval@gmail.com>
2023-06-12 01:26:12 -0700

    token for email verification

--- src/pages/welcomeForm.tsx
+++ src/pages/welcomeForm.tsx
@@ -3,25 +3,14 @@ import Head from "next/head";
 import { Button } from "~/components/ui/button";
 import { api } from "~/utils/api";
 import { RouteSignedOutUser } from "~/components/auth/routeSignedOutUser";
-// import { use, useEffect, useState } from "react";
-// import { sendLoginEmail } from "~/utils/nodeMailer";
-// import { useSession } from "next-auth/react";
 
 export const getServerSideProps = RouteSignedOutUser("/");
 
 const WelcomeForm: NextPage = () => {
-  // const { data: sessionData } = useSession();
-
-  //block page if user is not signed in
-
-
-
-
-  //gets current user info.
+  
   const { data: emailVerified, isLoading } =
     api.emailAPI.userEmailVerificationStatus.useQuery();
 
-  //Send confirmation email.
   const { mutate } = api.emailAPI.sendConfirmationEmail.useMutation();
 
   console.log(emailVerified);
--- src/utils/nodeMailer.ts
+++ src/utils/nodeMailer.ts
@@ -2,43 +2,25 @@
 import nodemailer from "nodemailer";
 import { env } from "~/env.mjs";
 
+export async function sendLoginEmail(email: string, token: string) {
 
-
-// async..await is not allowed in global scope, must use a wrapper
-export async function sendLoginEmail( email: string ) {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  // const testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    // port: 587,
+    service: "gmail",
     auth: {
-        user: env.GOOGLE_EMAIL,
-        pass: env.GOOGLE_PASS
-    }
+      user: env.GOOGLE_EMAIL,
+      pass: env.GOOGLE_PASS,
+    },
   });
 
- 
-
-  // send mail with defined transport object
   const info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: email, // list of receivers
-    subject: "Sending with mutation", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
+    from: '"Fred Foo 👻" <foo@example.com>', 
+    to: email,
+    subject: "Sending with mutation",
+    text: "Hello world?", 
+    html: `<b>Click here to activate your account: http://localhost:3000/verify/${token}</b>`, 
   });
 
+  //testing stuff
   console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
-
-//this is just for testing and causes it to send an email on server start
-// sendLoginEmail().catch(console.error);
-
