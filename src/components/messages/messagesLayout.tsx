e881207632857310670e2a3164d1c44fbbf406d4
samuelahmed <samueladuval@gmail.com>
2023-06-20 22:26:15 -0700

    remove contact nav items

--- src/components/messages/messagesLayout.tsx
+++ src/components/messages/messagesLayout.tsx
@@ -1,29 +1,29 @@
 import { ContactsNav } from "./contactsNav";
 
-const contactNavItems = [
-  {
-    title: "Profile",
-    name: "John Doe",
-    user: "meow",
-  },
-  {
-    title: "Account",
-    name: "Jane Smith",
-    user: "meow",
-  },
-  {
-    title: "Notifications",
-    name: "Mr Blue",
-    user: "meow",
-  },
-];
+// const contactNavItems = [
+//   {
+//     title: "Profile",
+//     name: "John Doe",
+//     user: "meow",
+//   },
+//   {
+//     title: "Account",
+//     name: "Jane Smith",
+//     user: "meow",
+//   },
+//   {
+//     title: "Notifications",
+//     name: "Mr Blue",
+//     user: "meow",
+//   },
+// ];
 
 export default function MessagesLayout() {
 
 
   return (
     <>
-      <ContactsNav items={contactNavItems} />
+      <ContactsNav  />
     </>
   );
 }
