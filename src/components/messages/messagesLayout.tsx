ed50641a6b769e9b02c21ae17a2acdf9421a4ee3
samuelahmed <samueladuval@gmail.com>
2023-06-21 17:53:18 -0700

    remove hardcode data

--- src/components/messages/messagesLayout.tsx
+++ src/components/messages/messagesLayout.tsx
@@ -1,29 +1,9 @@
 import { ContactsNav } from "./contactsNav";
 
-// const contactNavItems = [
-//   {
-//     title: "Profile",
-//     name: "John Doe",
-//     user: "meow",
-//   },
-//   {
-//     title: "Account",
-//     name: "Jane Smith",
-//     user: "meow",
-//   },
-//   {
-//     title: "Notifications",
-//     name: "Mr Blue",
-//     user: "meow",
-//   },
-// ];
-
 export default function MessagesLayout() {
-
-
   return (
     <>
-      <ContactsNav  />
+      <ContactsNav />
     </>
   );
 }
