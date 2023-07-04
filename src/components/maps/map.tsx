695b0aeca3099b7af8b7efa0845cf7c470b28465
samuelahmed <samueladuval@gmail.com>
2023-07-03 17:44:27 -0700

    add address and button to update map location on create care session

--- src/components/maps/map.tsx
+++ src/components/maps/map.tsx
@@ -1,49 +1,60 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import { env } from "~/env.mjs";
-import { useEffect } from "react";
-
-const containerStyle = {
-  width: "100%",
-  height: "100%",
-};
-
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-};
-
+import { use, useEffect } from "react";
+//import places library
+import { getGeocode, getLatLng } from "use-places-autocomplete";
+import { useState } from "react";
+import { Button } from "../ui/button";
 interface GoogleMapsProps {
   googleAddress: string;
 }
 
 const GoogleMaps: React.FC<GoogleMapsProps> = ({ googleAddress }) => {
-  console.log(googleAddress, " from google maps");
+  const [lat, setLat] = useState(37.33548);
+  const [lng, setLng] = useState(-121.893028);
+
+  const handleAddress = async () => {
+    if (!googleAddress) return;
+
+    const results = await getGeocode({ address: googleAddress });
+
+    if (results && results.length > 0 && results[0]) {
+      const { lat, lng } = getLatLng(results[0]);
+      setLat(lat);
+      setLng(lng);
+    }
+  };
+
+  const containerStyle = {
+    width: "100%",
+    height: "100%",
+  };
+  // 37.3189069 -121.9932247
+  const center = {
+    lat: lat,
+    lng: lng,
+  };
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    // libraries: ["places"],
   });
-
-  useEffect(() => {
-    if (isLoaded && googleAddress) {
-      // Perform any action or fetch data based on the googleAddress prop
-      console.log(googleAddress, " from GoogleMaps useEffect");
-    }
-  }, [isLoaded, googleAddress]);
-
-  // useEffect(() => {
-  //   // Perform any action or fetch data based on the googleAddress prop
-  //   console.log(googleAddress, " from GoogleMaps useEffect");
-  // }, [googleAddress]);
 
   if (!isLoaded) return <div>Loading...</div>;
 
   return (
     <>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-      ></GoogleMap>
+      <div className="h-full w-full ">
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+        ></GoogleMap>
+        <Button variant="outline" onClick={handleAddress}>
+          Update Address
+        </Button>
+      </div>
     </>
   );
 };
