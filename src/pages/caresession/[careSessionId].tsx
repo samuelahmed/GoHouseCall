f8318903a566d6d30fe1d44d2639d04a686352f2
samuelahmed <samueladuval@gmail.com>
2023-07-06 15:44:53 -0700

    setup care session dynamic route

---
+++ src/pages/caresession/[careSessionId].tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+export const getStaticPaths = async () => {
+  //get all care sessions to generate static pages
+  //map over all the sessions
+  //return the paths object with the params object
+};
+
+const CareSession: NextPage = () => {
+  return <></>;
+};
+
+export default CareSession;
