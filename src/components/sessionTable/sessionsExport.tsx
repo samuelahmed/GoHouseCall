e2fa4943ad629fb167d5d2e8313f12c40fdb30e5
samuelahmed <samueladuval@gmail.com>
2023-07-11 12:36:41 -0700

    remove create session button from caregivers

--- src/components/sessionTable/sessionsExport.tsx
+++ src/components/sessionTable/sessionsExport.tsx
@@ -1,22 +1,27 @@
 import { useRouter } from "next/router";
 import { Button } from "~/components/ui/button";
 import SessionsTableMain from "./sessionsTableMain";
+import { api } from "~/utils/api";
 
 export default function SessionsExport() {
   const router = useRouter();
+  const { data: user } = api.careSessionAPI.me.useQuery();
 
   return (
     <>
-      <div className="flex items-center justify-between px-5 py-4">
-        <Button
-          variant="outline"
-          onClick={() => {
-            console.log(router.push("/createSession"));
-          }}
-        >
-          Create Session
-        </Button>
-      </div>
+      {user?.type === "patient" && (
+        <div className="flex items-center justify-between px-5 py-4">
+          <Button
+            variant="outline"
+            onClick={() => {
+              console.log(router.push("/createSession"));
+            }}
+          >
+            Create Session
+          </Button>
+        </div>
+      )}
+
       <SessionsTableMain />
     </>
   );
