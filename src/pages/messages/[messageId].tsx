cd03dbe0949a6342e4bb14c7c9d408248fa3e6e6
samuelahmed <samueladuval@gmail.com>
2023-07-13 17:47:09 -0700

    rebuild messages start

--- src/pages/messages/[messageId].tsx
+++ src/pages/messages/[messageId].tsx
@@ -2,18 +2,35 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { RouteSignedOutAndNewUsers } from "~/components/auth/routeSignedOutAndNewUsers";
 import { ContactsNav } from "~/components/messages/contactsNav";
+import { useRouter } from "next/router";
+import { api } from "~/utils/api";
 
 export const getServerSideProps = RouteSignedOutAndNewUsers("/offline/messages");
 
 
 
 const Messages: NextPage = () => {
+
+  const router = useRouter();
+  const id = router.query.profileId;
+
+  // const { data: currentMessages } = api
+
+
+  
+
+
   return (
     <>
       <Head>
         <title>Messages</title>
         <meta name="description" content="Edit your House Call account" />
       </Head>
+
+
+
+
+
       <div className="h-screen">
         <ContactsNav />
       </div>
