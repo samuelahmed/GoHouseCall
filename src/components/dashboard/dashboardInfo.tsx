94d7685632da689ebcd77a11f16679355eeb5f93
samuelahmed <samueladuval@gmail.com>
2023-07-17 16:03:34 -0700

    outline new dashboard style

--- src/components/dashboard/dashboardInfo.tsx
+++ src/components/dashboard/dashboardInfo.tsx
@@ -6,50 +6,76 @@ export default function Dashboardinfo() {
   return (
     <>
       <div className="flex min-w-full flex-col space-y-2 px-4 py-4 md:flex-row md:space-x-2 md:space-y-0">
-        <div className="w-full">
-          <Card className="shadow">
+        <div className="h-screen max-h-50vh min-h-50vh w-full">
+          <Card className="h-full shadow">
             <CardHeader>
-              <CardTitle>Sessions</CardTitle>
+              <CardTitle>Scheduled Sessions</CardTitle>
+            </CardHeader>
+            <CardContent>meow list of upcoming sessions</CardContent>
+          </Card>
+        </div>
+
+        <div className="h-screen max-h-50vh min-h-50vh w-full">
+          <Card className="h-full shadow">
+            <CardHeader>
+              <CardTitle>Monthly Overview</CardTitle>
             </CardHeader>
             <CardContent>
-              <p>Scheduled Sessions: 3</p>
+              <p className="pb-2 text-lg">
+                Earnings: <span className="">$532</span>
+              </p>
+              <p
+                onClick={() => {
+                  //add filter to only show completed sessions
+                  void router.push("/sessions");
+                }}
+                className=" hover:cursor-pointer"
+              >
+                <span className="underline">Completed Sessions</span>: 5
+              </p>
+              <p
+                onClick={() => {
+                  //add filter to only show completed sessions
+                  void router.push("/sessions");
+                }}
+                className=" hover:cursor-pointer"
+              >
+                <span className="underline">Applied Sessions</span>: 9
+              </p>
+              <p
+                onClick={() => {
+                  //add filter to only show completed sessions
+                  void router.push("/sessions");
+                }}
+                className=" hover:cursor-pointer"
+              >
+                <span className="underline">Canceled Sessions</span>: 2
+              </p>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="h-screen max-h-50vh min-h-50vh w-full">
+          <Card className="h-full shadow">
+            <CardHeader>
+              <CardTitle>Account</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p
+                onClick={() => {
+                  void router.push("/messages/noContactSelected");
+                }}
+                className="underline hover:cursor-pointer"
+              >
+                Messages
+              </p>
               <p
                 onClick={() => {
                   void router.push("/sessions");
                 }}
                 className="underline hover:cursor-pointer"
               >
-                Applied Sessions: 5
+                Sessions
               </p>
-              <p>Completed Sessions: 6</p>
-            </CardContent>
-          </Card>
-        </div>
-        <div className="w-full">
-        <Card className="shadow">
-            <CardHeader>
-              <CardTitle>Messages</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p
-                onClick={() => {
-                  void router.push("/messages");
-                }}
-                className="underline hover:cursor-pointer"
-              >
-                Unread Messages: 5
-              </p>
-              <p>Sent Messages: 3</p>
-              <p>Total Contacts: 6</p>
-            </CardContent>
-          </Card>
-        </div>
-        <div className="w-full">
-        <Card className="shadow">
-            <CardHeader>
-              <CardTitle>Account</CardTitle>
-            </CardHeader>
-            <CardContent>
               <p
                 onClick={() => {
                   void router.push("/settings/profile");
@@ -61,51 +87,19 @@ export default function Dashboardinfo() {
               <p
                 className="underline hover:cursor-pointer"
                 onClick={() => {
-                  void router.push("/settings/account");
-                }}
-              >
-                Payments
-              </p>
-              <p
-                className="underline hover:cursor-pointer"
-                onClick={() => {
                   void router.push("/settings/notifications");
                 }}
               >
                 Notifications
               </p>
-            </CardContent>
-          </Card>
-        </div>
-      </div>
-      <div className="flex flex-col space-y-4 px-4">
-        <div className="w-full">
-        <Card className="shadow">
-            <CardHeader>
-              <CardTitle className="text-center">Scheduled Sessions</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>Session Info</p>
-            </CardContent>
-          </Card>
-        </div>
-        <div className="w-full">
-        <Card className="shadow">
-            <CardHeader>
-              <CardTitle className="text-center">Applied Sessions</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>Session Info</p>
-            </CardContent>
-          </Card>
-        </div>
-        <div className="w-full">
-        <Card className="shadow">
-            <CardHeader>
-              <CardTitle className="text-center">Completed Sessions</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>Session Info</p>
+              <p
+                className="underline hover:cursor-pointer"
+                onClick={() => {
+                  void router.push("/settings/payments");
+                }}
+              >
+                Payments
+              </p>
             </CardContent>
           </Card>
         </div>
